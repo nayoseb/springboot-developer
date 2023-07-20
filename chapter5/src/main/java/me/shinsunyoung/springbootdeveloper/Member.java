d0e44375b2c41ab189e94c5eae2054b396204e97
nayoseb <115917642+nayoseb@users.noreply.github.com>
2023-07-20 20:06:31 +0900

    chapter5

--- chapter5/src/main/java/me/shinsunyoung/springbootdeveloper/Member.java
+++ chapter5/src/main/java/me/shinsunyoung/springbootdeveloper/Member.java
@@ -6,13 +6,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //기본 생성자
 @AllArgsConstructor
 @Getter
 @Entity
 public class Member {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키를 자동으로 1씩 증가
     @Column(name = "id", updatable = false)
     private Long id;
     @Column(name = "name", nullable = false)
